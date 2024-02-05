ab0c563b5825fa82598de3c977990c61426e19f4
Adriano Lima <dri.limasantos@gmail.com>
2024-02-05 17:22:45 -0300

    up

--- sorteio_novo_institucional/views.py
+++ sorteio_novo_institucional/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     	return render(request, 'sorteio_novo_institucional/home.html')
+
+def solucoes(request):
+    	return render(request, 'sorteio_novo_institucional/solucoes.html')
+
+def orcamento(request):
+    	return render(request, 'sorteio_novo_institucional/orcamento.html')
