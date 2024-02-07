dd0b4595c01ab0af58b6921c07186dda2b67ddf8
Adriano Lima <dri.limasantos@gmail.com>
2024-02-07 16:26:45 -0300

    up

--- setup/settings.py
+++ setup/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
