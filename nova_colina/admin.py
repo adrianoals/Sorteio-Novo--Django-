4f152b067b3e5f858a4d85130a151fb0ae2656b5
Adriano Lima <dri.limasantos@gmail.com>
2024-02-20 16:13:14 -0300

    up

--- nova_colina/admin.py
+++ nova_colina/admin.py
@@ -1,25 +1,28 @@
 from django.contrib import admin
-from nova_colina.models import Apartamento, VagaSimples, VagaDupla, Sorteio
+from nova_colina.models import Apartamento, Sorteio, GrupoVaga
 
 
 class ApartamentoAdmin(admin.ModelAdmin):
     list_display = ('id', 'numero_apartamento')
     list_display_links = ('id', 'numero_apartamento')
 
-class VagaSimplesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'vaga_simples')
-    list_display_links = ('id', 'vaga_simples')
+class GrupoVagaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vagas')
+    list_display_links = ('id', 'vagas')
 
-class VagaDuplaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'vaga_dupla')
-    list_display_links = ('id', 'vaga_dupla')
+# class VagaSimplesAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'vaga_simples')
+#     list_display_links = ('id', 'vaga_simples')
+
+# class VagaDuplaAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'vaga_dupla')
+#     list_display_links = ('id', 'vaga_dupla')
 
 class SorteioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'apartamento', 'vaga_simples', 'vaga_dupla')
-    list_display_links = ('id', 'apartamento', 'vaga_simples', 'vaga_dupla')
+    list_display = ('id', 'apartamento', 'vagas')
+    list_display_links = ('id', 'apartamento', 'vagas')
 
 
 admin.site.register(Apartamento, ApartamentoAdmin)
-admin.site.register(VagaSimples, VagaSimplesAdmin)
-admin.site.register(VagaDupla, VagaDuplaAdmin)
+admin.site.register(GrupoVaga, GrupoVagaAdmin)
 admin.site.register(Sorteio, SorteioAdmin)
--- popular_db.py
+++ popular_db.py
@@ -1,5 +1,5 @@
 # Importar os modelos
-from nova_colina.models import Apartamento, VagaSimples, VagaDupla
+from nova_colina.models import Apartamento, GrupoVaga
 
 # Função para popular Apartamentos
 def popular_apartamentos():
@@ -7,17 +7,12 @@ def popular_apartamentos():
         Apartamento.objects.get_or_create(numero_apartamento=numero[0])
 
 # Função para popular Vagas Simples
-def popular_vagas_simples():
-    for vaga in VagaSimples.VAGA_SIMPLES_CHOICES:
-        VagaSimples.objects.get_or_create(vaga_simples=vaga[0])
+def popular_vagas():
+    for vaga in GrupoVaga.VAGAS_CHOICES:
+        GrupoVaga.objects.get_or_create(vagas=vaga[0])
 
-# Função para popular Vagas Duplas
-def popular_vagas_duplas():
-    for vaga in VagaDupla.VAGA_DUPLA_CHOICES:
-        VagaDupla.objects.get_or_create(vaga_dupla=vaga[0])
 
 # Executar as funções
 if __name__ == "__main__":
     popular_apartamentos()
-    popular_vagas_simples()
-    popular_vagas_duplas()
+    popular_vagas()
