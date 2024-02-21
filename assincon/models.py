743a0cc3c6ff0eeae6f6ffe99be57deaea2dc6e2
Adriano Lima <dri.limasantos@gmail.com>
2024-02-21 10:38:38 -0300

    up

--- assincon/models.py
+++ assincon/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class ListaDePresenca(models.Model):
+    nome = models.CharField(max_length=150,)
+    def __str__(self):
+        return f'{self.nome}'
