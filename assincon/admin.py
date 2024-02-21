350e4cb5e0b31b960d330378c66c9213d982e189
Adriano Lima <dri.limasantos@gmail.com>
2024-02-21 14:55:52 -0300

    up

--- assincon/admin.py
+++ assincon/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from assincon.models import ListaPresenca
 
-# Register your models here.
+class ListaPresencaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'brinde')
+    list_display_links = ('id', 'nome', 'brinde')	
+
+admin.site.register(ListaPresenca, ListaPresencaAdmin)
+
--- assincon/models.py
+++ assincon/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class ListaDePresenca(models.Model):
+class ListaPresenca(models.Model):
     nome = models.CharField(max_length=150,)
+    brinde = models.BooleanField(default=False)
     def __str__(self):
         return f'{self.nome}'
